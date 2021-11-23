76ba2f095e2bbf473f5a3024482ddd17b15494ed
SergioHPassos <shp360@hotmail.com>
2021-11-24 00:51:50 +0900

    RESTful API init foundation

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var hbs = require("hbs");
-
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var hbs = require("hbs");
+require('./app_api/models/db');
 
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
 var roomsRouter = require("./app_server/routes/rooms");
+var apiRouter = require('./app_api/routes/index');
 
 const { hasSubscribers } = require("diagnostics_channel");
 
@@ -38,6 +39,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
 app.use("/rooms", roomsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,64 @@
-var fs = require("fs");
-var tripsData = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
 
-/* GET travel view */
-const travel = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways", tripsData });
+/* Controller that creates API request and sends it off to the app_api, then gets a response back */
+
+const { response } = require('express');
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+
+
+/* render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+
+    // if the result was not an array, make it an array and report error
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in database';
+        }
+    }
+
+    // render the page
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
 };
+
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    // construct the request
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    // send the request to the api and get a response. Use the response to render the travel page
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log('statusCode: ', response && response.statusCode)
+            renderTravelList(req, res, body);
+        }
+    )
+};
+
 
 module.exports = {
-  travel,
-};
+    travelList
+}
